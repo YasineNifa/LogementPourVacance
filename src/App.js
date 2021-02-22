82d9954d1ca2dda56d7c40a49975abc1769c7218
Yassine Nifa <yasine.nifa@gmail.com>
2021-02-22 15:56:13 +0100

    Header done

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Header from './Header'
+import Home from './Home'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Home/>
+      
+      {/* Home */}
+          {/* header */}
+          {/* banner serach dates, slider */}
+          {/* Cards */}
+          {/* footer */}
+      {/* Search page */}
+          {/* ... */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Banner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Banner.css'
+function Banner() {
+    return (
+        <div className='banner'>
+            
+        </div>
+    )
+}
+
+export default Banner
